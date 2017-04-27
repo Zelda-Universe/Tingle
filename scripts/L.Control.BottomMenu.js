e19d769a9ad0cffc04974e536de0e8cb02bd7981
Jason Rappaport <jason@squareknot.com>
2017-04-27 16:58:44 -0400

    Wip

--- scripts/L.Control.BottomMenu.js
+++ scripts/L.Control.BottomMenu.js
@@ -4,7 +4,7 @@ L.Control.BottomMenu = L.Control.extend({
         width: window.innerWidth,
         height: window.innerHeight,
         delay: '0',
-        openTo: 16,
+        openTo: 24,
     },
     _category: '',
     _open: false,

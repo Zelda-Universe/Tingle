b0bf5ce1ad1bc2b990fb017edbbcfc92b4830c58
Pysis868 <Pysis868@Gmail.com>
2018-01-01 22:08:59 -0500

    Looking through the search issue with not being able to find the recommended shrine.
    I feel it is related to the data, so I found some interesting pieces that were not complete from the previous backups given to me, and now with my own prod db access, was able to get a refreshed, more filled version.
    
    I wrote plenty of queries, expressions, and commands to get most of the data I needed, and sanitize the rest.
    For this, I enhanced and refactored the dev db export script.
    There are now tons of options to choose from and debug with as well!
    Also threw in some nifty code folding since there is plenty there now.
    I realized late this essentially turned into a couple of steps in a custom-built build tool.
    Should now have result messages, more streamlined output, debug wait, verbose, captured/hooked user input, intelligent cleaning, and colors!!
    Kept the old features like dry run testing, and now works pretty well now that I've learned to use MySQL's config editor feature.
    
    As far as the data goes, I removed the assumption that marker_tab is now needed based on the query that gets markers for the map UI.
    I also generate a bunch of user accounts with fake data (sanitized!!) so any links that are also needed for that main monster marker query also exist.
    These are mainly from the marker table now, which seems to be the largest source of user ids, with any stragglers from marker_tab.
    I do not limit visibility so as to incorporate any possible future scenarios a developer might want.
    Some others pieces of data were updated too, like the troublesome markers were replaced with better ones, like the missing shrine, and/or it had the visibility toggled off, so now we have a more authoritative version checked in.
    However, there are these hack messages sprinkled into the data that maybe should not be there.  Anyway, we can discuss the file and improve upon it more now that it has some source control, instead of just messing around with production data O_o.
    
    Changed the names to also match the production database convention to be helpful.
    
    It's a pretty nice system now that can be run as needed in the future, especially from a local PC with an SSH tunnel and further setup mysql login configuration!
    
    Updated the development and install sections related to database work on how to more completely utilize this script's features, and some other useful commands and shortcuts/tips.

--- dev/db/exportDatabaseForDev.sh
+++ dev/db/exportDatabaseForDev.sh
@@ -1,74 +1,215 @@
 #!/usr/bin/env bash
 
-statusPrint() {
-  [[ "$QUIET" != "true" ]] && echo -n $*;
-}
+## Function Library
+{
+  statusPrint() {
+    [[ "$QUIET" != "true" ]] && echo -en $*;
+  }
 
-pauseWhenEnabled() {
-  [[ "$PAUSE" == "true" ]] && read;
-}
+  pauseWhenEnabled() {
+    [[ "$PAUSE" == "true" ]] && read -s -n 1;
+  }
 
-runCommandAndPrintStatus() {
-  commandString="$*";
-  # echo $commandString; exit 1;
-  if [[ "$DRY_RUN" == "false" ]]; then
-    if eval "$commandString"; then
-      statusPrint -e "Success\n";
-    else
-      statusPrint -e "Failed\n";
+  issueStep() {
+    if [[ "$#" -eq "1" ]]; then
+      commandString="$1";
+    elif [[ "$#" -eq "2" ]]; then
+      description="$1";
+      commandString="$2";
     fi
-  else
-    statusPrint "Command: $commandString";
-  fi
+
+    if [[ "$VERBOSE" == "true" ]]; then
+      statusPrint "\n${BROWN_ORANGE}Command${NC}: ${DARK_GREY}$commandString${NC}";
+    fi
+
+    issueCommand "$commandString";
+
+    if [[ -n "$description" ]]; then
+      statusPrint "${BLUE} >${NC} $(echo -n "$description" | $messageRedirectionString)";
+      [[ "$BRIEF_MESSAGES" == "true" && "${#description}" -gt "$availableMessageCharacters" ]] && statusPrint "...";
+    fi
+
+    pauseWhenEnabled;
+  }
+
+  issueCommand() {
+    commandString="$1";
+
+    if [[ "$DRY_RUN" == "false" ]]; then
+      if eval "$commandString"; then
+        statusPrint "\n[${GREEN}Success${NC}] ";
+      else
+        lastCommandStatus="$?";
+
+        statusPrint "\n[${RED}Failed ${NC}] ";
+
+        if [[ "$CLEAN_ON_FAILURE" == "true" ]]; then
+          clean;
+        fi
+
+        if [[ "$FAIL_FAST" == "true" ]]; then
+          exit "$lastCommandStatus";
+        fi
+      fi
+    else
+      statusPrint "\n[${DARK_GREY}Not Run${NC}] ";
+    fi
+  }
+
+  clean() {
+    issueStep \
+      "Cleaning the no longer needed intermediate SQL files." \
+      "rm -f $allIntermediateFilePaths" \
+    ;
+  }
+
+  cleanAndExit() {
+    exitStatusToApply="${1:-2}";
+    statusPrint "\n";
+    clean;
+    statusPrint "Exiting...";
+    exit "$exitStatusToApply";
+  }
 }
 
-SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+## Initialization
+{
+  # Source: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+  BROWN_ORANGE='\033[0;33m';
+  LIGHT_GREY='\033[0;37m';
+  DARK_GREY='\033[1;30m';
+  BLUE='\033[0;34m';
+  GREEN='\033[0;32m';
+  RED='\033[0;31m';
+  NC='\033[0m'; # No Color
 
-[[ -z "$QUIET" ]] && QUIET="false";
-[[ -z "$PAUSE" ]] && PAUSE="false";
-[[ -z "$DRY_RUN" ]] && DRY_RUN="false";
+  SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+}
 
-dbUsername="zmaps";
-[[ -z "$dbPassword" ]] && read -s -p "Enter the database account password: " dbPassword; echo -n;
-database="zelda_maps";
-noAITables="user user_completed_marker";
-credentialOptions="-u $dbUsername -p$dbPassword";
-ignoreTablesOptions='';
-for table in $noAITables; do ignoreTablesOptions+="--ignore-table='$database.$table' "; done;
-structureOptions="--no-data";
-dataOptions="--no-create-info --skip-add-drop-table --skip-extended-insert";
+## Main Environment Configuration
+{
+  [[ -z "$QUIET" ]] && QUIET="false";
+  [[ -z "$VERBOSE" ]] && VERBOSE="false";
+  [[ -z "$BRIEF_MESSAGES" ]] && BRIEF_MESSAGES="false";
+  [[ -z "$PAUSE" ]] && PAUSE="false";
+  [[ -z "$DRY_RUN" ]] && DRY_RUN="false";
+  [[ -z "$FAIL_FAST" ]] && FAIL_FAST="true";
+  [[ -z "$CLEAN_ON_FAILURE" ]] && CLEAN_ON_FAILURE="true";
 
-keepAIFilePath="$SDIR/zelda_maps-structure_with-AI.sql";
-toRemoveAIFilePath="$SDIR/zelda_maps-structure_to-remove-AI.sql";
-aiRemovedFilePath="$SDIR/zelda_maps-structure_AI-removed.sql";
-dataFilePath="$SDIR/zelda_maps-data.sql";
-completeFilePath="$SDIR/zelda_maps-complete.sql";
-# TODO: eval the ignore params or all command line so tables get ignored???
+  [[ -z "$DB_NAME" ]] && DB_NAME="zeldamaps";
+  [[ -z "$MYSQL_OTHER_CONNECTION_OPTIONS" ]] && MYSQL_OTHER_CONNECTION_OPTIONS="";
+  [[ -z "$MYSQL_CONNECTION_STRING" ]] && MYSQL_CONNECTION_STRING="$MYSQL_OTHER_CONNECTION_OPTIONS -u'$MYSQL_USER' -p'$MYSQL_PASS'";
+}
 
-pauseWhenEnabled;
-statusPrint "Exporting majority of the database structure, keeping the auto increment values to match the data later on...";
-runCommandAndPrintStatus "mysqldump $credentialOptions $structureOptions $ignoreTablesOptions --add-drop-database --databases $database > '$keepAIFilePath' 2>/dev/null";
+## Derived Configuration & Internal Variables
+{
+  # `marker_tab` looked like user data that could be skipped, but apparently not, as no markers appeared using the current query 'monster' :X
+  noAITables="user user_completed_marker";
+  ignoreTablesOptions='';
+  for table in $noAITables; do ignoreTablesOptions+="--ignore-table='$DB_NAME.$table' "; done;
+  structureOptions="--no-data";
+  dataOptions="--no-create-info --skip-add-drop-table --skip-extended-insert";
+  if [[ "$VERBOSE" == "true" ]]; then
+    errorRedirectionString="";
+    mysqlVerboseString="-v";
+  else
+    errorRedirectionString="2>/dev/null";
+    mysqlVerboseString="";
+  fi
+  if [[ "$BRIEF_MESSAGES" == "true" ]]; then
+    availableMessageCharacters="$(expr "$(tput cols)" - 12 - 3)"; # For the result messages, their wrapping characters, the message prefix, and then the ellipsis.
+    messageRedirectionString="head --bytes=$availableMessageCharacters";
+  else
+    messageRedirectionString="cat";
+  fi
+  # Additional SQL query function files to use but not modify.
+  generateDevUsersQueryFilePath="$SDIR/exportDatabaseForDev-generateDevUsers.sql";
+  sqlizeSedFilePath="$SDIR/exportDatabaseForDev-sqlize.sed";
 
-pauseWhenEnabled;
-statusPrint "Exporting the rest of the database the structure, with no way to immediately remove the auto increment values to match not having data later on...";
-runCommandAndPrintStatus "mysqldump $credentialOptions $structureOptions $database $noAITables > '$toRemoveAIFilePath' 2>/dev/null";
+  # Intermediate, temporary, working files to be deleted at the end of the process.
+  keepAIFilePath="$SDIR/zeldamaps-structure_with-AI.sql";
+  toRemoveAIFilePath="$SDIR/zeldamaps-structure_to-remove-AI.sql";
+  aiRemovedFilePath="$SDIR/zeldamaps-structure_AI-removed.sql";
+  dataFilePath="$SDIR/zeldamaps-data.sql";
+  sanitizedPartialUserDataFilePath="$SDIR/zeldamaps-sanitizedPartialUserData.txt";
+  generatedDevUserDataFilePath="$SDIR/zeldamaps-devUserData.sql";
 
-pauseWhenEnabled;
-statusPrint "Exporting majority of the database data, ignoring certain tables with more sensitive user or other data...";
-runCommandAndPrintStatus "mysqldump $credentialOptions $dataOptions $ignoreTablesOptions $database > '$dataFilePath' 2>/dev/null";
+  allIntermediateFilePaths="'$keepAIFilePath' '$toRemoveAIFilePath' '$aiRemovedFilePath' '$dataFilePath' '$sanitizedPartialUserDataFilePath' '$generatedDevUserDataFilePath'";
 
-pauseWhenEnabled;
-statusPrint "Removing auto increment values in sensitive data table schemas...";
-runCommandAndPrintStatus "sed -r 's| AUTO_INCREMENT=[[:digit:]]+||g' '$toRemoveAIFilePath' > '$aiRemovedFilePath'";
+  # Resultant, generated file to keep at the end of the process.
+  # completeFilePath="$SDIR/zeldamaps-complete.sql";
+  completeFilePath="$SDIR/zeldamaps.sql"; # Since the it is already version-tracked, we can just overwrite the main file now.
+}
 
-pauseWhenEnabled;
-statusPrint "Cleaning intermediate SQL file...";
-runCommandAndPrintStatus "rm '$toRemoveAIFilePath'";
+## Main Program Flow
+{
+  trap 'statusPrint "\n${RED}User cancels process.${NC}"; cleanAndExit 2;' INT KILL TERM STOP;
 
-pauseWhenEnabled;
-statusPrint "Combining intermediate SQL files into a single convenient script for later import and version control...";
-runCommandAndPrintStatus "cat '$keepAIFilePath' '$aiRemovedFilePath' '$dataFilePath' > '$completeFilePath'";
+  issueStep \
+    "Exporting majority of the database structure, keeping the auto increment values to match the data later on." \
+    "mysqldump $MYSQL_CONNECTION_STRING $mysqlVerboseString $structureOptions $ignoreTablesOptions --add-drop-database --databases $DB_NAME > '$keepAIFilePath' $errorRedirectionString" \
+  ;
 
-pauseWhenEnabled;
-statusPrint "Cleaning the no longer needed intermediate SQL files...";
-runCommandAndPrintStatus "rm '$keepAIFilePath' '$aiRemovedFilePath' '$dataFilePath'";
+  issueStep \
+    "Exporting the rest of the database structure, with no way to immediately remove the auto increment values to match not having data later on." \
+    "mysqldump $MYSQL_CONNECTION_STRING $mysqlVerboseString $structureOptions $DB_NAME $noAITables > '$toRemoveAIFilePath' $errorRedirectionString" \
+  ;
+
+  issueStep \
+    "Exporting majority of the database data, ignoring certain tables with more sensitive user or otherwise less useful data for development." \
+    "mysqldump $MYSQL_CONNECTION_STRING $mysqlVerboseString $dataOptions $ignoreTablesOptions $DB_NAME > '$dataFilePath' $errorRedirectionString" \
+  ;
+
+
+  issueStep \
+    "Exporting and sanitizing only the required user records by id, with their visiblity and level data for later use in development so that all markers can be displayed." \
+    "mysql $MYSQL_CONNECTION_STRING $mysqlVerboseString $DB_NAME > '$sanitizedPartialUserDataFilePath' $errorRedirectionString < '$generateDevUsersQueryFilePath'" \
+  ;
+
+  if [[ "$VERBOSE" == "true" ]]; then
+    issueStep \
+      "Removing embedded verbose query." \
+      "sed -i '1,27d' '$sanitizedPartialUserDataFilePath'" \
+    ;
+  fi
+
+  issueStep \
+    "Preparing the generated user data into a SQL format." \
+    "sed -i -r -f '$sqlizeSedFilePath' '$sanitizedPartialUserDataFilePath'" \
+  ;
+
+  issueStep \
+    "Writing SQL INSERT header." \
+    "echo 'INSERT INTO \`user\`' > '$generatedDevUserDataFilePath'" \
+  ;
+
+  issueStep \
+    "Writing header data fields." \
+    "head -n 1 '$sanitizedPartialUserDataFilePath' >> '$generatedDevUserDataFilePath'" \
+  ;
+  issueStep \
+    "Writing intermediate VALUES term." \
+    "echo 'VALUES' >> '$generatedDevUserDataFilePath'" \
+  ;
+  issueStep \
+    "Writing data fields." \
+    "tail -n +2 '$sanitizedPartialUserDataFilePath' >> '$generatedDevUserDataFilePath'" \
+  ;
+  #   "tail -n +2 dev/db/userLevels.txt | sed -r 's|([[:digit:]]+)\s+([[:digit:]]+)\s+([[:digit:]]+)|\(\1, \'test\1\', \'test\1\', \2, \3\)|g'";
+  issueStep \
+    "Writing terminating colon." \
+    "echo ';' >> '$generatedDevUserDataFilePath'" \
+  ;
+
+
+  issueStep \
+    "Removing auto increment values in sensitive data table schemas." \
+    "sed -r 's| AUTO_INCREMENT=[[:digit:]]+||g' '$toRemoveAIFilePath' > '$aiRemovedFilePath'" \
+  ;
+
+  issueStep \
+    "Combining intermediate SQL files into a single convenient script for later import and version control." \
+    "cat '$keepAIFilePath' '$aiRemovedFilePath' '$dataFilePath' '$generatedDevUserDataFilePath' > '$completeFilePath'" \
+  ; # "cat '$keepAIFilePath' '$aiRemovedFilePath' '$dataFilePath' > '$completeFilePath'" \
+  clean;
+}

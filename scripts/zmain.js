816ccb5a19436668e5364a19f096ee09616feb49
KokoroSenshi <dwl_chong@hotmail.com>
2018-02-12 22:38:48 +1100

    Fixed my own typo

--- scripts/zmain.js
+++ scripts/zmain.js
@@ -7,7 +7,7 @@
 
 var OSName="Unknown OS";
 if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-if (navigator.appVersion.indexOf("Mac")!=-1) O  SName="MacOS";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
 if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
 if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 

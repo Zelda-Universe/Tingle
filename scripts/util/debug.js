42af12b50a578498e1cf844b7a2f996ac1610782
Pysis868 <Pysis868@Gmail.com>
2018-04-15 22:53:14 -0400

    Upgrading debugging capabilities. Added:  - Colors!!!!  Really helps look through a lot of messages quickly.  - Relative call level.  - Execution flow delimiters.  - Simple/short function param display.  - Short string param display.  - Ignore function tracing.  - Optional param new line.  - Quick color log function that utilizes some native browsers' color output API. Organized.

--- scripts/util/debug.js
+++ scripts/util/debug.js
@@ -1,10 +1,31 @@
+// Configure below
+
 var isDebugMode = false;
 
-var objectsToTrace = [];
+var objectsToTrace = [
+  L.Control.ZLayers,
+  L.Control.ZLayersBottom
+];
+
+var methodsToIgnore = {
+  "L.Control.ZLayersBottom": [
+    "_animate",
+    "drawerTop"
+  ]
+};
+
+var debugOptions = {
+  abbvFn: true,
+  argNewLines: false,
+  stringMax: 20,
+  ignore: methodsToIgnore
+};
+
+// Configure above
 
 if(isDebugMode) {
   applyFunctionTraceToObjects(
     objectsToTrace,
-    { abbvFn: true }
+    debugOptions
   );
 }
--- scripts/util/debug.lib.js
+++ scripts/util/debug.lib.js
@@ -1,9 +1,10 @@
+var callLevel = 0;
+
 function debugPrint() {
   if(isDebugMode) {
     console.debug(arguments);
   }
 };
-
 
 function applyFunctionTraceToObjects(objectsToTrace, options) {
   objectsToTrace.forEach(function(objectToTrace) {
@@ -14,37 +15,78 @@ function applyFunctionTraceToObjects(objectsToTrace, options) {
 // TODO: Would we want to debug instance methods along with the class' prototype's methods as well?
 // If so, refactor into a function doing 2 passes, 1 for each set.
 // https://stackoverflow.com/a/38581438/1091943
-function inject(object, beforeFn, options) {
+function inject(object, extraFn, options = {}) {
   let objectPrototype = object.prototype;
+  let objectName = (
+    objectPrototype._className ||
+    object._className ||
+    object._debugName ||
+    object.displayName ||
+    object.name
+  );
 
   for (let propName of Object.getOwnPropertyNames(objectPrototype)) {
     let prop = objectPrototype[propName];
-    if(typeof prop === "function") {
+    if(typeof prop === "function" && (
+      !options.ignore ||
+      !options.ignore[objectName] ||
+      !options.ignore[objectName].includes(propName)
+    )) {
       objectPrototype[propName] = (function(fnName) {
         return function() {
-          beforeFn.call(
+          extraFn.call(
             this,
-            (
-              objectPrototype._className ||
-              object._className ||
-              object._debugName ||
-              object.displayName ||
-              object.name
-            ),
+            true,
+            objectName,
             fnName,
             options,
             arguments
           );
-          return prop.apply(this, arguments);
+
+          let returnValue = prop.apply(this, arguments);
+
+          extraFn.call(
+            this,
+            false,
+            objectName,
+            fnName,
+            options,
+            arguments
+          );
         };
       })(propName);
     }
   }
 };
 
-function logFnCall(className, name, options, args) {
-  if(!options) options = {};
+function logFnCall(before, className, fnName, options = {}, args) {
   if(options.abbvFn === undefined) options.abbvFn = true;
+  if(options.argNewLines === undefined) options.argNewLines = false;
+  if(options.colors === undefined) options.colors = true;
+  if(options.stringMax === undefined) options.stringMax = 20;
+
+  let colors = {
+    obj: "yellow",
+    class: "orange",
+    fn: "skyblue",
+    punct: "dark-grey",
+    term: "white"
+  };
+
+  if(callLevel == 0 && before) {
+    quickColorLog('%c--- %cStart of Monitored Execution %c---', colors.punct, colors.fn, colors.punct);
+  }
+
+  var colorList = [];
+
+  let positionText = ( (before)
+    ? "%cEntering %c>>>"
+    : "%cLeaving  %c<<<"
+  );
+  colorList = colorList.concat([
+    ((before) ? "green" : "brown"),
+    colors.punct
+  ]);
 
   let objectName = (
     this._debugName ||
@@ -52,18 +94,77 @@ function logFnCall(className, name, options, args) {
     this.name
   );
 
-  let headerString = objectName + ': ' + className + '.' + name + '(';
+  if(before) callLevel++;
+
+  let headerString = '' +
+    '(' + callLevel + ') ' +
+    positionText + ' ' +
+    '%c' + objectName + ': ' +
+    '%c' + className + '.' +
+    '%c' + fnName + '%c('
+  ;
+  colorList = colorList.concat([
+    colors.obj,
+    colors.class,
+    colors.fn,
+    colors.punct
+  ]);
+
+  if(!before) callLevel--;
 
   let argsString = Array.prototype.map.call(args, function(arg) {
-    if(typeof arg === "function" && options.abbvFn)
-      return Object.prototype.toString.call(arg);
-    else
-      return String(arg);
-  }).join(', ');
-  
-  if(argsString) argsString = '\n\t' + argsString + '\n';
+    let result;
 
-  let footerString = ')';
+    colorList.push(colors.term); // For the leading color
 
-  console.log(headerString + argsString + footerString);
+    if(typeof arg === "function" && options.abbvFn) {
+      result = Object.prototype.toString.call(arg);
+    } else {
+      result = String(arg);
+      if(result.length > options.stringMax) {
+        result = result.substr(0, options.stringMax - 3) + "..."; // Took away punct color string.  Ellipsis could go either way to match the term or the punct.
+        // colorList.push(colors.punct);
+      }
+    }
+
+    colorList.push(colors.punct); // For the joined comma below.
+
+    return '%c' + result;
+  }).join('%c, ');
+
+  if(argsString) {
+    colorList.pop();
+
+    if(options.argNewLines) {
+      argsString = '\n\t' + argsString + '\n';
+    }
+  }
+
+  let footerString = '%c)';
+  colorList.push(colors.punct);
+
+  let finalString = '' +
+    headerString +
+    argsString +
+    footerString
+  ;
+
+  if(!options.colors) finalString.replace("%c", ' ');
+
+  quickColorLog.apply(console, [finalString].concat(colorList));
+
+  if(callLevel == 0 && !before) {
+    quickColorLog('%c---  %cEnd  of Monitored Execution %c---', colors.punct, colors.fn, colors.punct);
+  }
+};
+
+function quickColorLog() {
+  console.log.apply(
+    console,
+    [arguments[0]].concat(
+      Array.prototype.slice.call(arguments, 1).map(function(color) {
+        return "color: " + color + ";";
+      })
+    )
+  );
 };

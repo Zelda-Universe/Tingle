b062833334ded05aea9c4ffe2d94c9065797d395
Jason Rappaport <jason@squareknot.com>
2018-05-21 17:18:13 -0400

    revert

--- scripts/widgets/Logo.js
+++ scripts/widgets/Logo.js
@@ -13,28 +13,3 @@ Logo.prototype._initDOMElements = function(opts) {
   logoDiv.style.height = (opts.headerHeight - 2) + 'px'; // Need to remove 2px because of the separator
   logoDiv.style.textAlign = 'center';//TODO: move to CSS
 };
-
-Logo.prototype._initDOMElements = function(opts) {
-  this.domNode = $('' +
-    '<ul class="category-selection-list">' +
-    '</ul>'
-  );
-  this.menuEntryContainerTemplate = '' +
-    '<li class="category-selector">' +
-    '</li>'
-  ;
-  this._addCategoryMenuEntry(
-    new CategoryButtonCompleted({
-        onToggle: opts.onCompletedToggle,
-        toggledOn: opts.showCompleted
-      }
-    )
-  );
-  // this.categoryButtonCompleted.domNode.on('toggle', opts.onCompletedToggle.bind(this.categoryButtonCompleted));
-};
- 
-Logo.prototype._addCategoryMenuEntry = function(categoryButton) {
-  var menuEntry = $(this.menuEntryContainerTemplate);
-  menuEntry.append(categoryButton.domNode);
-  this.domNode.append(menuEntry);
-};

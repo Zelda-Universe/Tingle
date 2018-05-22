4115d81ded4f28859cac6aceedcd81b329be55ee
Jason Rappaport <jason@squareknot.com>
2018-05-22 15:44:18 -0400

    wip

--- scripts/widgets/Logo.js
+++ scripts/widgets/Logo.js
@@ -13,10 +13,52 @@ Logo.prototype._initDOMElements = function(opts) {
   logoDiv.style.height = (opts.headerHeight - 2) + 'px'; // Need to remove 2px because of the separator
 };
 
-this._addCategoryMenuEntry(
-new CategoryButtonCompleted({
-    onToggle: opts.onCompletedToggle,
-    toggledOn: opts.showCompleted
-  }
-)
-);
+function CategoryMenu(opts) {
+  this._initDOMElements(opts);
+};
+
+CategoryMenu.prototype._initDOMElements = function(opts) {
+  this.domNode = $('' +
+    '<ul class="category-selection-list">' +
+    '</ul>'
+  );
+  this.menuEntryContainerTemplate = '' +
+    '<li class="category-selector">' +
+    '</li>'
+  ;
+
+  this._addCategoryMenuEntry(
+    new CategoryButtonCompleted({
+        onToggle: opts.onCompletedToggle,
+        toggledOn: opts.showCompleted
+      }
+    )
+  );
+  // this.categoryButtonCompleted.domNode.on('toggle', opts.onCompletedToggle.bind(this.categoryButtonCompleted));
+
+  var currentCategoryParentButton;
+  categoryTree.forEach(function(category) {
+    currentCategoryParentButton = new CategoryParentButton({
+      category: category,
+      onToggle: opts.onCategoryToggle
+    });
+
+    this._addCategoryMenuEntry(currentCategoryParentButton);
+
+    category.children.forEach(function(childCategory) {
+      currentChildCategoryButton = new CategoryButton({
+        category: childCategory,
+        onToggle: opts.onCategoryToggle
+      });
+
+      this._addCategoryMenuEntry(currentChildCategoryButton);
+      currentCategoryParentButton.addChild(currentChildCategoryButton);
+    }, this);
+  }, this);
+};
+
+CategoryMenu.prototype._addCategoryMenuEntry = function(categoryButton) {
+  var menuEntry = $(this.menuEntryContainerTemplate);
+  menuEntry.append(categoryButton.domNode);
+  this.domNode.append(menuEntry);
+};

00a28a7fa2bcc9d33266c2a79c3245c6ddd10601
Jason Rappaport <jason@squareknot.com>
2018-05-22 07:56:21 -0400

    wip

--- scripts/widgets/AccountButton.js
+++ scripts/widgets/AccountButton.js
@@ -6,11 +6,8 @@ function AccountButton(opts) {
 };
 
 AccountButton.prototype._initDOMElements = function() {
-  this.domNode = $('' +
-    '<a class="button account-button full-icon-space" href="#account">' +
-      '<i class="fas fa-user"></i>' +
-    '</a>'
-  );
+  this.domNode = L.DomUtil.create('a', 'button fas fa-user account-button full-icon-space');
+  this.domNode.href = "#account";
 };
 
 AccountButton.prototype._setupUserInputListener = function(opts) {

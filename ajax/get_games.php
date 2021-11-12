f41eeebd5f84d758fe1abed54a868f79c0c500b5
Pysis <Pysis868@Gmail.com>
2021-11-12 00:07:28 -0500

    Fixed get games API
    Wonder how an error arrived in this file I did not work on and I should be based of latest code, but fixed anyway..

--- ajax/get_games.php
+++ ajax/get_games.php
@@ -2,8 +2,6 @@
    $path = DIRNAME(__FILE__);
    include("$path/../config.php");
 
-   $map = $_GET["game"];
-    
    $query = "select c.id
                   , c.short_name as shortName
                   , c.name
@@ -13,16 +11,16 @@
              order by c.id
    ;";
    //echo $query;
-    
+
    $result = @$mysqli->query($query);
 
 	if(!$result) {
 		print($mysqli->error);
 		return;
 	}
-   
+
    $res = array();
-   
+
    while ($row = $result->fetch_assoc()) {
       array_push($res, $row);
    }

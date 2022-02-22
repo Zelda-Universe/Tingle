3fc357700d10d90ba3d97163f8b39fafa08e0080
Pysis <Pysis868@Gmail.com>
2022-02-22 16:37:56 -0500

    Res minify updates
    May have been broken, plus difficult to deal with having SELinux, so refactored and cleaned some.

--- res.php
+++ res.php
@@ -1,90 +1,109 @@
 <?php
   $path=__DIR__;
 
-    if(!isset($_GET["type"])) {
-      print("A type must be provided! javascript or css.");
-      exit;
-    }
+  if(!isset($_GET["type"])) {
+    print("A type must be provided! javascript or css.");
+    exit;
+  }
 
-    include_once("$path/config.php");
+  include_once("$path/config.php");
 
-    $type = strtolower($_GET['type']);
-    $type = preg_replace("#[^a-z]#","",$type);
-    if($type!="javascript" &&
-        $type!="css") {
-        print("Invalid type! ($type)");
-        return;
-    }
-    $mtype = "text/$type";
-    $ext = ".css";
+  $type = strtolower($_GET['type']);
+  $type = preg_replace("#[^a-z]#","",$type);
+  if($type!="javascript" &&
+    $type!="css") {
+    print("Invalid type! ($type)");
+    return;
+  }
+  $mtype = "text/$type";
+  $ext = ".css";
 
-    if($type=="javascript") {
-        $ext = ".js";
-    }
+  if($type=="javascript") {
+    $ext = ".js";
+  }
 
-    if(!file_exists("$path/cache")) mkdir("$path/cache");
+  header("Content-Type: $mtype");
 
-    header("Content-Type: $mtype");
+  $localRegistryFile="$path/$type.ini";
+  $cacheRegistryFile="$cacheFolder/$type.ini";
+  $cacheDestFile="$cacheFolder/index$ext";
 
-    $data = parse_ini_file("$path/$type.ini");
-    $update = false;
-    if(file_exists("$path/cache/index$ext") && !isset($_GET['update'])) {
-        foreach($data as $file=>$time) {
-            if(stripos($file,"//")!==false || !file_exists("$path/$file")) {
-                continue;
-            }
-            if($time!=filemtime("$path/$file")) {
-                $update=true;
-                break;
-            }
+  $update=false;
+  if(!file_exists("$cacheRegistryFile")) {
+    $update=true;
+    copy("$localRegistryFile", "$cacheRegistryFile") or die("Error: Cache file problem with: $cacheRegistryFile");
+  }
+
+  # Mark for updating the cache or not based on old or absent timestamps.
+  $data = parse_ini_file("$cacheRegistryFile");
+
+  if(!$update) {
+    if(!isset($_GET['update'])) {
+      foreach($data as $resfile=>$time) {
+        if(
+          !stripos($resfile,"//") ||
+          !file_exists("$path/$resfile")
+        ) {
+          continue;
         }
+        if($time != filemtime("$path/$resfile")) {
+          $update=true;
+          break;
+        }
+      }
     } else {
-        $update=true;
+      $update=(strtolower($_GET['update']) === "true");
     }
-    if($update) {
-        // print('DEBUG: Updating cached file...<br>');
-        include "$path/lib/minify.php";
-        $output = "/* cache/index$ext */\n";
-        foreach($data as $file=>$time) {
-          // print('DEBUG: Processing file entry'.$file.'...<br>');
-            $fpath = $file;
-            if(stripos($file,"//")===true) continue;
-            $fpath = "$path/$file";
-            if(!file_exists($fpath)) {
-                $output.="/* $file doesn't exist */\n";
-                continue;
-            }
-            $output.="/* Source: $file */\n";
-            $filedata = file_get_contents($fpath);
-            if($minify) {
-              if($type=="javascript") {
-                // print('DEBUG: Minifying javascript...<br>');
-                  $filedata = minify_js($filedata);
-              } else {
-                // print('DEBUG: Minifying css...<br>');
-                  $filedata = minify_css($filedata);
-              }
-            }
-            $output.="$filedata\n";
-            if(stripos($file,"//")===false) {
-                $data[$file]=filemtime($fpath);
-            }
-        }
-        file_put_contents("$path/cache/index$ext",$output);
-        header("Content-Length: ".strlen($output));
-        header("X-Updated: true");
-        print $output;
+  }
 
-        $output = "";
-        foreach($data as $file=>$time) {
-            $output.="$file=$time\n";
+  # Update the minified cache file.
+  if($update) {
+    // error_log('DEBUG: Updating cached file...<br>');
+    include "$path/lib/minify.php";
+    $output = "/* index$ext */\n";
+    foreach($data as $resfile=>$time) {
+      // error_log('DEBUG: Processing file entry'.$resfile.'...<br>');
+      $rfpath = $resfile;
+      if(stripos($resfile,"//")) continue;
+      $rfpath = "$path/$resfile";
+      if(!file_exists($rfpath)) {
+          $output.="/* $resfile doesn't exist */\n";
+          continue;
+      }
+      $output.="/* Source: $resfile */\n";
+      $resfiledata = file_get_contents($rfpath);
+      if($minify) {
+        if($type=="javascript") {
+          // error_log('DEBUG: Minifying javascript...<br>');
+          $resfiledata = minify_js($resfiledata);
+        } else {
+          // error_log('DEBUG: Minifying css...<br>');
+          $resfiledata = minify_css($resfiledata);
         }
-        file_put_contents("$path/$type.ini",$output);
-        return;
+      }
+      $output.="$resfiledata\n";
+      if(stripos($resfile,"//")===false) {
+        $data[$resfile]=filemtime($rfpath);
+      }
     }
-    
-    $out = file_get_contents("$path/cache/index$ext");
-    
-    header("Content-Length: ".strlen($out));
-    header("X-Updated: false");
-    print $out;
+    # Save and send the minified file.
+    file_put_contents("$cacheDestFile", $output);
+    header("Content-Length: ".strlen($output));
+    header("X-Updated: true");
+    print $output;
+
+    # Store timestamps for freshly-chached resource files in the registry file.
+    $output = "";
+    foreach($data as $resfile=>$time) {
+      $output.="$resfile=$time\n";
+    }
+    file_put_contents("$cacheRegistryFile", $output);
+    return;
+  }
+
+  $output = file_get_contents("$cacheDestFile");
+
+  header("Content-Length: ".strlen($output));
+  header("X-Updated: false");
+  print($output);
+?>

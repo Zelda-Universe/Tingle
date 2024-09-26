e898155e739ca4fb0a8ff26dfc02593482cffcc8
Pysis <Pysis868@Gmail.com>
2024-09-26 19:23:56 -0400

    Add container code
    to allow container access, but still hide them in listings.

--- ajax/get_container.php
+++ ajax/get_container.php
@@ -11,40 +11,44 @@
   $map = $_GET["game"];
   // debug_log("map: $map");
 
-  $query = 'select id
-                  , name
-                  , short_name as shortName
-                  , icon       as icon
-                  , marker_url as markerURL
-                  , marker_ext as markerExt
-                  , background_color as bgColor
-                  , show_map_control as showMapControl
-                  , show_zoom_control as showZoomControl
-                  , default_pos_x as centerX
-                  , default_pos_y as centerY
-                  , bound_top_pos_x as boundTopX
-                  , bound_top_pos_y  as boundTopY
-                  , bound_bottom_pos_x as boundBottomX
-                  , bound_bottom_pos_y as boundBottomY
-                  , cluster_max_zoom as clusterMaxZoom
-                  , cluster_grid_size as clusterGridSize
-                  , default_zoom as defaultZoom
-                  , max_zoom as maxZoom
-                  , tile_size as tileSize
-                  , icon_width as iconWidth
-                  , icon_height as iconHeight
-                  , icon_small_width as iconSmallWidth
-                  , icon_height as iconSmallHeight
-                  , switch_icons_at_zoom as switchIconsAtZoom
-                  , scaleP  as scaleP 
-                  , scaleN  as scaleN 
-                  , offsetX as offsetX
-                  , offsetY as offsetY
-               from ' . $map_prefix . 'container c
-              where (c.id = \'' . $map . '\'
-                     or c.short_name = \'' . $map . '\')
-                and c.visible = 1;
-  ';
+  $query = "
+    SELECT id
+      , name
+      , short_name as shortName
+      , icon       as icon
+      , marker_url as markerURL
+      , marker_ext as markerExt
+      , background_color as bgColor
+      , show_map_control as showMapControl
+      , show_zoom_control as showZoomControl
+      , default_pos_x as centerX
+      , default_pos_y as centerY
+      , bound_top_pos_x as boundTopX
+      , bound_top_pos_y  as boundTopY
+      , bound_bottom_pos_x as boundBottomX
+      , bound_bottom_pos_y as boundBottomY
+      , cluster_max_zoom as clusterMaxZoom
+      , cluster_grid_size as clusterGridSize
+      , default_zoom as defaultZoom
+      , max_zoom as maxZoom
+      , tile_size as tileSize
+      , icon_width as iconWidth
+      , icon_height as iconHeight
+      , icon_small_width as iconSmallWidth
+      , icon_height as iconSmallHeight
+      , switch_icons_at_zoom as switchIconsAtZoom
+      , scaleP  as scaleP
+      , scaleN  as scaleN
+      , offsetX as offsetX
+      , offsetY as offsetY
+      , visible as visible
+    FROM ${map_prefix}container c
+    WHERE (
+      c.id = '${map}'
+      OR c.short_name = '${map}'
+    )
+    AND c.enabled = 1;
+  ";
   //echo $query;
   $result = @$mysqli->query($query);
 

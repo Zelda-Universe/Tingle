f8a7ba4512145afb58fbfbd6348c145e64daea53
Pysis <Pysis868@Gmail.com>
2024-09-27 01:43:31 -0400

    Fix pathConversion script
    back to direct subdir expectations.
    Add reverse method setting.

--- dev/assets/tiles/pathConversion.fish
+++ dev/assets/tiles/pathConversion.fish
@@ -12,22 +12,28 @@ set -l SDIR (readlink -f (dirname (status filename)));
 source "$SDIR/../../scripts/common/altPushd.fish";
 source "$SDIR/../../scripts/common/debugPrint.fish";
 
-test -z "$tilesDir";
-and set tilesDir "$argv[1]";
-test -z "$tilesDir";
-and set tilesDir (readlink -e "$SDIR/../../../tiles");
+test -z "$tilesDir"         ; # TODO: Make specific to fileRootDir
+and set tilesDir "$argv[1]" ;
+# test -z "$tilesDir";
+# and set tilesDir (readlink -e "$SDIR/../../../tiles");
 # debugPrint "tilesDir: $tilesDir";
+
+test -z "$tilesDir";
+and return 1;
+
+test -z "$reverse"      ;
+and set reverse 'false' ;
 
 altPushd "$tilesDir";
 
 echo "Processing \"$tilesDir\"...";
 
 # -printf "%f\n" \
-find \
-  -mindepth 3 \
-  -maxdepth 3 \
-  -type f \
-  -iname '*.png' \
+find              \
+  -mindepth 1     \
+  -maxdepth 1     \
+  -type f         \
+  -iname '*.png'  \
 | while read file;
   # debugPrint "file: $file";
 
@@ -37,42 +43,56 @@ find \
   #   | tr '_.' '\n'
   # );
 
+  # echo "$file"    \
+  # | sed -r 's|^./[^/]+/[^/]+/||' \
+  # | tr '_.' '\n'  \
+  # | read -L z x y;
   echo "$file"    \
-  | sed -r 's|^./[^/]+/[^/]+/||' \
-  | tr '_.' '\n'  \
+  | sed -r 's|^./||' \
+  | tr '_./' '\n'  \
   | read -L z x y;
   # debugPrint "x: $x";
   # debugPrint "y: $y";
   # debugPrint "z: $z";
 
-  set subDir (
-    echo "$file"    \
-    | grep -oP 's|^./[^/]+/[^/]+(?=/)||'
-  );
-  # debugPrint "subDir: $subDir";
+  # set subDir (
+  #   echo "$file"    \
+  #   | grep -oP 's|^./[^/]+/[^/]+(?=/)||'
+  # );
+  # # debugPrint "subDir: $subDir";
 
-  set fileName (
-    echo "$file"    \
-    | sed -r "s|^$subDir/||"
-  );
-  # debugPrint "fileName: $fileName";
+  # set fileName (
+  #   echo "$file"    \
+  #   | sed -r "s|^$subDir/||"
+  # );
+  # # debugPrint "fileName: $fileName";
 
-  altPushd "$subDir";
+  # altPushd "$subDir";
 
   if test \
         "$outputZoomFolders" = 'true' \
     -o  "$outputAxisFolders" = 'true'
 
-    test ! -d "$z";
-    and mkdir "$z";
+    if test "$reverse" != 'true'
+      test ! -d "$z";
+      and mkdir "$z";
+    end
 
     if test "$outputAxisFolders" = 'true'
-      test ! -d "$z/$x";
-      and mkdir "$z/$x";
 
-      set newFilePath {$z}/{$x}/{$y};
+      if test "$reverse" = 'true'
+        set newFilePath {$z}_{$x}_{$y};
+        # debugPrint "newFilePath: $newFilePath";
+      else
+        test ! -d "$z/$x";
+        and mkdir "$z/$x";
+
+        set newFilePath {$z}/{$x}/{$y};
+        # debugPrint "newFilePath: $newFilePath";
+      end
     else
       set newFilePath {$z}/{$x}_{$y};
+      # debugPrint "newFilePath: $newFilePath";
     end
   else
     # set newFilePath {$z}_{$x}_{$y}; # Rest NIY..
@@ -80,10 +100,11 @@ find \
   set newFilePath "$newFilePath.png";
   # debugPrint "newFilePath: $newFilePath";
 
-	mv "$fileName" "$newFilePath";
+  # echo mv "$subDir/$fileName" "$newFilePath";
+  mv "$file" "$newFilePath";
 	echo -n '.';
 
-  popd;
+  # popd;
 end
 
 popd;

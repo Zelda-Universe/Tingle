2a3b3f09ffb333dcda4c20ae1a7e384453a5ce18
Pysis <Pysis868@Gmail.com>
2025-01-21 16:38:33 -0500

    Add delete script
    for removing PII from the database, before the automatic feature is completed.

---
+++ dev/db/deletePII.fish
@@ -0,0 +1,103 @@
+#!/usr/bin/env fish
+
+# MIT Licensed
+# by Pysis(868)
+# https://choosealicense.com/licenses/mit/
+
+# Check for user entry, confirm deletion, manually read id,
+# then erase specified field values, and print data completed.
+
+# Input & Validation
+begin
+  test -z "$dbusername";
+  and read -P 'dbusername: ' dbusername;
+  test -z "$dbpw";
+  and read -s -P 'dbpw: ' dbpw;
+  test -z "$databaseName";
+  and read -s -P 'databaseName: ' databaseName;
+  test -z "$databaseName";
+  and set databaseName 'zeldamaps';
+
+  test -n "$connStr";
+  and set connStr (echo $connStr | tr ' ' '\n');
+  test -z "$usernameTarget";
+  and read -P 'usernameTarget: ' usernameTarget;
+
+  if test -z "$usernameTarget"
+    return;
+  end
+end
+
+# Print found records.
+if not mysql -B                 \
+    -u"$dbusername"             \
+    -p"$dbpw"                   \
+    --database="$databaseName"  \
+    $connStr                    \
+    -e "
+      SELECT *
+      FROM `user`
+      WHERE `username` = '$usernameTarget'
+    ;"  \
+  ;
+
+  return 1;
+end
+
+# Confirm deletion
+begin
+  echo;
+
+  read -P 'Really delete this account?: ' -n 1 choice;
+
+  echo;
+
+  if test "$choice" != 'y'
+    return;
+  end
+end
+
+# Delete
+begin
+  test -z "$id";
+  and read -P 'Id: ' id;
+
+  set timestamp "$(date '+%s')";
+
+  if not mysql -B               \
+    -u"$dbusername"             \
+    -p"$dbpw"                   \
+    --database="$databaseName"  \
+    $connStr                    \
+    -e "
+      UPDATE `user`
+      SET
+        `username`  = '<deleted-$timestamp>',
+        `password`  = '<deleted>'           ,
+        `name`      = '<deleted>'           ,
+        `email`     = '<deleted-$timestamp>',
+        `ip`        = '<deleted>'
+      WHERE `id`    = '$id'
+    ;"
+    return 2;
+  end
+end
+
+# Print confirming record not found now.
+begin
+  mysql -B                      \
+    -u"$dbusername"             \
+    -p"$dbpw"                   \
+    --database="$databaseName"  \
+    $connStr                    \
+    -e "
+      SELECT *
+      FROM `user`
+      WHERE `username` = '$usernameTarget'
+    ;"    \
+  ;
+  # | cat \
+end
+
+echo -n 'Completed at ';
+date '+%Y-%m-%d %H:%M:%S';
